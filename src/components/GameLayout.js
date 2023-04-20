7dc05a194d15c47d46de7e28c04b87051f994fbf
Nockiest <115572770+Nockiest@users.noreply.github.com>
2023-04-20 19:27:01 +0200

    2 files modified

--- src/components/GameLayout.js
+++ src/components/GameLayout.js
@@ -6,56 +6,66 @@ import '../../style.scss';
 //import { GameContext } from '../../App';
 
 function GameLayout(props) {
- // const { playerBullets, setPlayerBulletsNumber } = useContext(GameContext);
   let battelGroundHeight = 400;
   let battelGroundWidth = 1300;
   let layoutStyles = {
     height: '100vh',
-    width: '100%',
+   // width: '100%',
   };
   let battlegroundStyles = {
     height: `${battelGroundHeight}px`,
-    display: 'block',
-    border: 'brown 2px solid',
+    overflowX: 'hidden',
   };
   
   return (
-    <span style={layoutStyles}>
+    <div style={layoutStyles}>
       <BattelGround style={battlegroundStyles} height={battelGroundHeight} width={battelGroundWidth} gameData={props.gameData}/>
-      <div id="side-bar" onClick={() => props.gameData.changePlayerBulletsNumber(1)}>
-  {/* First row */}
-  {<div className="settings-row">
-    <div className="settings-col">{/*props.gameData.playerBullets*/}</div>
-    <div className="settings-col"></div>
-  </div>}
-  
-  {/* Second row */}
-  {<div className="info-row">
-    <div className="info-col">
-      <div>a</div>
-      <div>a</div>
+      <div id="lowerBarContainer"> 
+      <div id="reload-area"  >
+        <button onClick={() => props.gameData.changePlayerBulletsNumber(1)}>Reload</button>
+      </div>
+      <div id="side-bar"  >
+       <div className="column">a</div>
+       <div className="column">b</div>
+       <div className="column">c</div>
+       <div className="column">
+         <button onClick={() => props.gameData.changePlayerBulletsNumber(1)}>Next Wave</button>
+      </div>
+      </div>
+      </div>
     </div>
-    <div className="info-col">
-      <div>b</div>
-      <div>b</div>
-    </div>
-  </div>}
-  
-  {/* Third row */}
-  {<div className="buy-col">
-    <div className="buy-row">c</div>
-    <div className="buy-row">c</div>
-    <div className="buy-row">c</div>
-    <div className="buy-row">c</div>
-  </div>}
-  
-  {/* Fourth row */}
-  {<div className="button-row">
-    <button id="next-wave-button">Next Wave</button>
-  </div>}
-</div>
-</span>
   );
 }
 
 export default GameLayout;
+
+/* {/* First row *//*}*/
+/*{<div className="settings-row">
+<div className="settings-col">{props.gameData.playerBullets}</div>
+<div className="settings-col"></div>
+</div>}*/
+
+{/* Second row */}
+/*{<div className="info-row">
+<div className="info-col">
+  <div>a</div>
+  <div>a</div>
+</div>
+<div className="info-col">
+  <div>b</div>
+  <div>b</div>
+</div>
+</div>}*/
+
+{/* Third row */}
+/*{<div className="buy-col">
+<div className="buy-row">c</div>
+<div className="buy-row">c</div>
+<div className="buy-row">c</div>
+<div className="buy-row">c</div>
+</div>}*/
+
+{/* Fourth row */}
+/*{<div className="button-row">
+<button id="next-wave-button">Next Wave</button>
+</div>}}*/
